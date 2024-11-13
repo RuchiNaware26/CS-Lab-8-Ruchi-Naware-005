888a717f5401c22f97b0fd34aab930aea309d788
RuchiNaware26 <rnn@njit.edu>
2024-11-13 12:41:27 -0500

    Lab questions 1-2

---
+++ script2.js
@@ -0,0 +1,24 @@
+//Question 1//
+var  FirstName = "Ruchi";
+console.log(FirstName);
+
+
+var age = 18;
+console.log(age);
+
+//Question 2//
+//String//
+let Username = "Ruchi";
+console.log(Username);
+//Number//
+let age2 = 4965
+console.log(age2);
+//Boolean//
+let grade = false;
+console.log(grade);
+//Undefined//
+let house; 
+console.log(house);
+//Null//
+let house2 = null;
+console.log(house2);
